1a860c5e47cf37615c9ca9f44fd20858e723beb7
TAN WEI JIE, AMOS <amos.tan.2014@sis.smu.edu.sg>
2016-06-30 16:32:01 +0800

    added configroute and component var

--- src/classmentors/components/cohort/cohort.js
+++ src/classmentors/components/cohort/cohort.js
@@ -1,28 +1,28 @@
 /**
  * Created by AMOS on 30/6/16.
  */
-import module from 'classmentors/module.js';
+// import module from 'classmentors/module.js';
 import cohortTmpl from './cohort-view.html!text';
 // import './cohort.css!';
 
 // This here didn't work, not sure why - Amos
-// export const component = {
-//     cohortTmpl
-// };
+export const component = {
+    cohortTmpl
+};
 //
-// export function configRoute($routeProvider, routes){
-//     $routeProvider.when(routes.cohort, {
-//         template: cohortTmpl
-//     });
-// }
-// configRoute.$inject = ['$routeProvider', 'routes'];
-
-module.config([
-    '$routeProvider',
-    'routes',
-    function($routeProvider, routes){
-        console.log('SOMETHING HAPPENS HERE');
-        $routeProvider
-            .when(routes.cohort, {template: cohortTmpl})
-    }
-]);
+export function configRoute($routeProvider, routes){
+    $routeProvider.when(routes.cohort, {
+        template: cohortTmpl
+    });
+}
+configRoute.$inject = ['$routeProvider', 'routes'];
+//
+// module.config([
+//     '$routeProvider',
+//     'routes',
+//     function($routeProvider, routes){
+//         console.log('SOMETHING HAPPENS HERE');
+//         $routeProvider
+//             .when(routes.cohort, {template: cohortTmpl})
+//     }
+// ]);
