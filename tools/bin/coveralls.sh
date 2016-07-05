5fbd409dec99b841e34bb9678d0b07c6eaabb52f
Damien Lebrun <dinoboff@hotmail.com>
2016-07-05 14:05:59 +0100

    Fix coveralls command

--- tools/bin/coveralls.sh
+++ tools/bin/coveralls.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "Running test with coverage..."
-./tools/bin/coverage
+npm run cover
 
 echo "sending coverage report to coveralls..."
 cat ./coverage/lcov.info | coveralls && rm -rf ./coverage
