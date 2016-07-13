838f4383a9c817d5264bd9c6a17ae9548b1a1f05
Damien Lebrun <dinoboff@hotmail.com>
2016-07-13 13:40:59 +0100

    convert build cmd to shelljs

---
+++ tools/bin/build.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+'use strict';
+
+const sh = require('shelljs');
+const tools = require('@singpath/tools');
+const config = require('./config.js');
+
+sh.set('-e');
+
+// setup
+tools.clean(config.build.root);
+sh.echo(`Setting up "${config.build.app.root}/"...`);
+sh.mkdir('-p', config.build.app.root);
+sh.cp('-r', 'LICENSE', 'README.md', 'tools/assets/*', config.build.app.root);
+
+
+// Bundles
+const cmd = [
+  'jspm build',
+  `${config.build.app.main} - ${config.build.app.deps.names.join(' - ')}`
+].join(' ');
+const umdOpts = [
+  '--format umd',
+  `--global-name "${config.build.app.name}"`,
+  `--global-deps '${JSON.stringify(config.build.app.deps.map)}'`
+].join(' ');
+
+sh.echo(`Buidling ${config.build.app.js}...`);
+tools.exec(`${cmd} "${config.build.app.js}" ${umdOpts} --skip-source-maps`);
+
+sh.echo(`Buidling ${config.build.app.minJs}...`);
+tools.exec(`${cmd} "${config.build.app.minJs}" ${umdOpts} --minify`);
+
+
+// Dependency tree
+//
+// You don't want the browser to load the source map file
+// (the source map is often as big as the bundle).
+// So we are stripping the source map directive in the bundle
+// by stripping the last line.
+sh.echo(`Removing source map directive from ${config.build.app.minJs}...`);
+sh.sed('-i', /^\/\/# sourceMappingURL=.*$/, '', config.build.app.minJs);
+
+sh.echo('Analysing source map...');
+sh.exec(`source-map-explorer --html "${config.build.app.minJs}"{,.map}`, {silent: true})
+  .to(config.build.app.tree);
+sh.echo(`open "${config.build.app.tree}" to find weight of each modules.`);
+
+// Archive
+sh.echo(`Creating archive in "${config.build.archive}".`);
+tools.zip(config.build.app.root, config.build.archive).then(() => sh.echo('Archive created.'));
