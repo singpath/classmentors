1e4e99c294432140a7d07f25a1ec9196667c26be
TAN WEI JIE, AMOS <amos.tan.2014@sis.smu.edu.sg>
2016-07-19 01:38:03 +0800

    Created new challenge service factory

--- src/classmentors/components/challenges/challenges.js
+++ src/classmentors/components/challenges/challenges.js
@@ -11,14 +11,52 @@ import * as mcq from './mcq/mcq.js';
 export function configRoute($routeProvider, routes){
     $routeProvider
         .when(routes.viewMcq, {
-            template: mcq.showTmpl
+            template: mcq.showTmpl,
+            controllerAs: 'ctrl',
+            controller: mcq.someController
         });
 
 }
-
 configRoute.$inject = ['$routeProvider', 'routes'];
 
-//TODO: Clarify what should and should not be a component
-// export const component = {
-//
-// }
+//TODO: Generic save function
+export function challengeFactory($q, $route, spfAuthData, clmDataStore){
+    return {
+        saveTask : function(event, _, task, taskType, isOpen) {
+      var copy = spfFirebase.cleanObj(task);
+
+      if (taskType === 'linkPattern') {
+        delete copy.badge;
+        delete copy.serviceId;
+        delete copy.singPathProblem;
+      } else if (copy.serviceId === 'singPath') {
+        delete copy.badge;
+        if (copy.singPathProblem) {
+          copy.singPathProblem.path = spfFirebase.cleanObj(task.singPathProblem.path);
+          copy.singPathProblem.level = spfFirebase.cleanObj(task.singPathProblem.level);
+          copy.singPathProblem.problem = spfFirebase.cleanObj(task.singPathProblem.problem);
+        }
+      } else {
+        delete copy.singPathProblem;
+        copy.badge = spfFirebase.cleanObj(task.badge);
+      }
+
+      if (!copy.link) {
+        // delete empty link. Can't be empty string
+        delete copy.link;
+      }
+
+      self.creatingTask = true;
+      clmDataStore.events.addTask(event.$id, copy, isOpen).then(function() {
+        spfAlert.success('Task created');
+        $location.path(urlFor('editEvent', {eventId: self.event.$id}));
+      }).catch(function(err) {
+        $log.error(err);
+        spfAlert.error('Failed to created new task');
+      }).finally(function() {
+        self.creatingTask = false;
+      });
+    }
+    }
+}
+challengeFactory.$inject = ['$q', '$route', 'spfAuthData', 'clmDataStore'];
