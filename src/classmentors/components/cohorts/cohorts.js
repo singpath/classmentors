0e7c0a6ed066ddb591a7a6fd641ef409114f23ef
shiquan-lim <shiquan.lim@gmail.com>
2016-09-01 12:47:11 +0800

    Implemented charting for solution submission (still raw). closes #123

--- src/classmentors/components/cohorts/cohorts.js
+++ src/classmentors/components/cohorts/cohorts.js
@@ -788,26 +788,57 @@ function ClmCohortStatsPageCtrl(
         if(self.selectedStatistic) {
             // var dataString = JSON.parse('{"Sprint 1": [10, 20, 30, 40, 50],"Sprint 2": [2, 4, 6, 8, 10],"Sprint 3": [5, 10, 15, 20, 25]}');
             if(self.selectedStatistic == 'Submission time series') {
-                var dataObj = {"Code": [], "Text": [], "Link": []};
+                // How formatted data should look like
+                var dataObj = {};
+                // dataObj = {"setosa_x": [3.5, 3.0, 3.2, 3.1, 3.6, 3.9, 3.4, 3.4, 2.9, 3.1, 3.7, 3.4, 3.0, 3.0, 4.0, 4.4, 3.9, 3.5, 3.8, 3.8, 3.4, 3.7, 3.6, 3.3, 3.4, 3.0, 3.4, 3.5, 3.4, 3.2, 3.1, 3.4, 4.1, 4.2, 3.1, 3.2, 3.5, 3.6, 3.0, 3.4, 3.5, 2.3, 3.2, 3.5, 3.8, 3.0, 3.8, 3.2, 3.7, 3.3],
+                //     "versicolor_x": [3.2, 3.2, 3.1, 2.3, 2.8, 2.8, 3.3, 2.4, 2.9, 2.7, 2.0, 3.0, 2.2, 2.9, 2.9, 3.1, 3.0, 2.7, 2.2, 2.5, 3.2, 2.8, 2.5, 2.8, 2.9, 3.0, 2.8, 3.0, 2.9, 2.6, 2.4, 2.4, 2.7, 2.7, 3.0, 3.4, 3.1, 2.3, 3.0, 2.5, 2.6, 3.0, 2.6, 2.3, 2.7, 3.0, 2.9, 2.9, 2.5, 2.8],
+                //     "setosa": [0.2, 0.2, 0.2, 0.2, 0.2, 0.4, 0.3, 0.2, 0.2, 0.1, 0.2, 0.2, 0.1, 0.1, 0.2, 0.4, 0.4, 0.3, 0.3, 0.3, 0.2, 0.4, 0.2, 0.5, 0.2, 0.2, 0.4, 0.2, 0.2, 0.2, 0.2, 0.4, 0.1, 0.2, 0.2, 0.2, 0.2, 0.1, 0.2, 0.2, 0.3, 0.3, 0.2, 0.6, 0.4, 0.3, 0.2, 0.2, 0.2, 0.2],
+                //     "versicolor": [1.4, 1.5, 1.5, 1.3, 1.5, 1.3, 1.6, 1.0, 1.3, 1.4, 1.0, 1.5, 1.0, 1.4, 1.3, 1.4, 1.5, 1.0, 1.5, 1.1, 1.8, 1.3, 1.5, 1.2, 1.3, 1.4, 1.4, 1.7, 1.5, 1.0, 1.1, 1.0, 1.2, 1.6, 1.5, 1.6, 1.5, 1.3, 1.3, 1.3, 1.2, 1.4, 1.2, 1.0, 1.3, 1.2, 1.3, 1.3, 1.1, 1.3]};
+
+                var dataArr = [];
+                // dataArr = [["setosa_x", 3.5, 3.0, 3.2, 3.1, 3.6, 3.9, 3.4, 3.4, 2.9, 3.1, 3.7, 3.4, 3.0, 3.0, 4.0, 4.4, 3.9, 3.5, 3.8, 3.8, 3.4, 3.7, 3.6, 3.3, 3.4, 3.0, 3.4, 3.5, 3.4, 3.2, 3.1, 3.4, 4.1, 4.2, 3.1, 3.2, 3.5, 3.6, 3.0, 3.4, 3.5, 2.3, 3.2, 3.5, 3.8, 3.0, 3.8, 3.2, 3.7, 3.3],
+                //     ["versicolor_x", 3.2, 3.2, 3.1, 2.3, 2.8, 2.8, 3.3, 2.4, 2.9, 2.7, 2.0, 3.0, 2.2, 2.9, 2.9, 3.1, 3.0, 2.7, 2.2, 2.5, 3.2, 2.8, 2.5, 2.8, 2.9, 3.0, 2.8, 3.0, 2.9, 2.6, 2.4, 2.4, 2.7, 2.7, 3.0, 3.4, 3.1, 2.3, 3.0, 2.5, 2.6, 3.0, 2.6, 2.3, 2.7, 3.0, 2.9, 2.9, 2.5, 2.8],
+                //     ["setosa", 0.2, 0.2, 0.2, 0.2, 0.2, 0.4, 0.3, 0.2, 0.2, 0.1, 0.2, 0.2, 0.1, 0.1, 0.2, 0.4, 0.4, 0.3, 0.3, 0.3, 0.2, 0.4, 0.2, 0.5, 0.2, 0.2, 0.4, 0.2, 0.2, 0.2, 0.2, 0.4, 0.1, 0.2, 0.2, 0.2, 0.2, 0.1, 0.2, 0.2, 0.3, 0.3, 0.2, 0.6, 0.4, 0.3, 0.2, 0.2, 0.2, 0.2],
+                //     ["versicolor", 1.4, 1.5, 1.5, 1.3, 1.5, 1.3, 1.6, 1.0, 1.3, 1.4, 1.0, 1.5, 1.0, 1.4, 1.3, 1.4, 1.5, 1.0, 1.5, 1.1, 1.8, 1.3, 1.5, 1.2, 1.3, 1.4, 1.4, 1.7, 1.5, 1.0, 1.1, 1.0, 1.2, 1.6, 1.5, 1.6, 1.5, 1.3, 1.3, 1.3, 1.2, 1.4, 1.2, 1.0, 1.3, 1.2, 1.3, 1.3, 1.1, 1.3]];
+
+                //Axis data object
+                var axisParam = {};
+                // axisParam = {
+                //     setosa: 'setosa_x',
+                //     versicolor: 'versicolor_x'
+                // };
+
+                //Initialise dataObj
+                for(var e = 0; e < self.cohort.events.length; e++) {
+                    dataObj[self.cohort.events[e] + "_x"] = [];
+                    dataObj[self.cohort.events[e]] = [];
+                    axisParam[self.cohort.events[e]] = self.cohort.events[e] + "_x";
+                }
+
+                console.log(dataObj);
+
                 spfFirebase.loadedArray(['classMentors/userActions'], {
-                    orderByChild: 'action',
-                    equalTo: 'submitLinkResponse'
+                    // orderByChild: 'action',
+                    // equalTo: 'submitLinkResponse'
                 }).then(function(promise) {
                     return promise;
                 }).then(function(data) {
-                    self.allLogs = data;
-                    console.log(self.allLogs);
+                    self.submissionLogs = data;
                 }).catch(function (err) {
                     $log.error(err);
                     return err;
                 }).then(function () {
-                    if(self.selectedStatistic = 'Submissions') {
-                        for(var actionIndex = 0; actionIndex < self.allLogs.length; actionIndex++) {
-                            // if(self.allLogs[actionIndex].action == 'submitLinkResponse') {
-                            //     dataObj.Link.push(action.)
-                            // }
-                            console.log(new Date(self.allLogs[actionIndex].timestamp));
+                    for(var actionIndex = 0; actionIndex < self.submissionLogs.length; actionIndex++) {
+                        var logHolder = self.submissionLogs[actionIndex];
+                        if(self.cohort.events.indexOf(logHolder.eventId) >= 0) {
+                            dataObj[logHolder.eventId + "_x"].push(logHolder.timestamp);
+                            dataObj[logHolder.eventId].push(new Date(logHolder.timestamp).getHours());
+                            // console.log(new Date(logHolder.timestamp).getDate());
                         }
+                        // if(self.allLogs[actionIndex].action == 'submitLinkResponse') {
+                        //     dataObj.Link.push(action.)
+                        // }
+                        // console.log(new Date(self.submissionLogs[actionIndex].timestamp));
                     }
                 }).then(function () {
                     // var canvas = d3.select('#canvas').node(),
@@ -901,31 +932,30 @@ function ClmCohortStatsPageCtrl(
                     //         type: "spline"
                     //     }
                     // });
+                    for(var obj in dataObj) {
+                        var newArr = [obj];
+                        dataArr.push(newArr.concat(dataObj[obj]));
+                        console.log(newArr.concat(dataObj[obj]));
+                    }
+                    console.log(dataArr);
+                    console.log(axisParam);
+                    console.log(dataObj);
                     var chart = c3.generate({
                         bindto: "#chart",
                         data: {
-                            xs: {
-                                setosa: 'setosa_x',
-                                versicolor: 'versicolor_x',
-                            },
-                            // iris data from R
-                            columns: [
-                                ["setosa_x", 3.5, 3.0, 3.2, 3.1, 3.6, 3.9, 3.4, 3.4, 2.9, 3.1, 3.7, 3.4, 3.0, 3.0, 4.0, 4.4, 3.9, 3.5, 3.8, 3.8, 3.4, 3.7, 3.6, 3.3, 3.4, 3.0, 3.4, 3.5, 3.4, 3.2, 3.1, 3.4, 4.1, 4.2, 3.1, 3.2, 3.5, 3.6, 3.0, 3.4, 3.5, 2.3, 3.2, 3.5, 3.8, 3.0, 3.8, 3.2, 3.7, 3.3],
-                                ["versicolor_x", 3.2, 3.2, 3.1, 2.3, 2.8, 2.8, 3.3, 2.4, 2.9, 2.7, 2.0, 3.0, 2.2, 2.9, 2.9, 3.1, 3.0, 2.7, 2.2, 2.5, 3.2, 2.8, 2.5, 2.8, 2.9, 3.0, 2.8, 3.0, 2.9, 2.6, 2.4, 2.4, 2.7, 2.7, 3.0, 3.4, 3.1, 2.3, 3.0, 2.5, 2.6, 3.0, 2.6, 2.3, 2.7, 3.0, 2.9, 2.9, 2.5, 2.8],
-                                ["setosa", 0.2, 0.2, 0.2, 0.2, 0.2, 0.4, 0.3, 0.2, 0.2, 0.1, 0.2, 0.2, 0.1, 0.1, 0.2, 0.4, 0.4, 0.3, 0.3, 0.3, 0.2, 0.4, 0.2, 0.5, 0.2, 0.2, 0.4, 0.2, 0.2, 0.2, 0.2, 0.4, 0.1, 0.2, 0.2, 0.2, 0.2, 0.1, 0.2, 0.2, 0.3, 0.3, 0.2, 0.6, 0.4, 0.3, 0.2, 0.2, 0.2, 0.2],
-                                ["versicolor", 1.4, 1.5, 1.5, 1.3, 1.5, 1.3, 1.6, 1.0, 1.3, 1.4, 1.0, 1.5, 1.0, 1.4, 1.3, 1.4, 1.5, 1.0, 1.5, 1.1, 1.8, 1.3, 1.5, 1.2, 1.3, 1.4, 1.4, 1.7, 1.5, 1.0, 1.1, 1.0, 1.2, 1.6, 1.5, 1.6, 1.5, 1.3, 1.3, 1.3, 1.2, 1.4, 1.2, 1.0, 1.3, 1.2, 1.3, 1.3, 1.1, 1.3],
-                            ],
+                            xs: axisParam,
+                            columns: dataArr,
                             type: 'scatter'
                         },
                         axis: {
                             x: {
-                                label: 'Sepal.Width',
+                                label: 'timestamp',
                                 tick: {
                                     fit: false
                                 }
                             },
                             y: {
-                                label: 'Petal.Width'
+                                label: 'Date of Action'
                             }
                         }
                     });
