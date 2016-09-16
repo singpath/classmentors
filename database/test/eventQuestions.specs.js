41ab5e38fbf401754813f8548b9452501238d150
Damien Lebrun <dinoboff@hotmail.com>
2016-09-16 14:39:20 +0100

    Set eventQuestions questions read rules

--- database/test/eventQuestions.specs.js
+++ database/test/eventQuestions.specs.js
@@ -5,48 +5,50 @@ const expect = chai.expect;
 const utils = require('./utils.js');
 const auth = utils.auth;
 
-/*
-eventQuestions ->
-$eventKey ->
-questions -> "auth !== null && root.child('classMentors/events').child($eventId).child('owner/publicId').val() === root.child('auth/users').child(auth.uid).child('publicId').val()"
-$questionKey ->
-answers ->
-$publicId ->
-comments ->
-$commentKey ->
-$commentor$ID -> “My comment”
-*/
 
-describe.skip('eventQuestions', function() {
+describe('eventQuestions', function() {
 
   beforeEach(function() {
     utils.setFirebaseData();
   });
 
-  describe('rules', function() {
-    const eventId = 'some-event-id';
-    const bobEventId = 'bob-event-id';
-    const passwordHash = 'x'.repeat(64);
+  describe('read rules', function() {
+    const eventId = 'alice-event';
+    const questionPath = `classMentors/eventQuestions/${eventId}/questions`;
 
-    it('should allow the admin to read and write to eventQuestions', function() {
-      expect(auth.admin).can.write({'test':'test'}).path('classMentors/eventQuestions');
+    it('should allow the admin to read eventQuestions', function() {
       expect(auth.admin).can.read.path('classMentors/eventQuestions');
     });
 
-    it('should allow the event owner to read questionOwners', function() {
-      // TODO: Find a matching rule and unit test.
-      //expect(auth.bob).can.read.path('classMentors/eventQuestions/'+bobEventId+'/questionOwners');
+    it('should allow the event owner to read eventQuestions', function() {
+      expect(auth.alice).can.read.path(questionPath);
     });
 
-    it('should allow the event owner to read and write to eventQuestions', function() {
-      // expect(auth.bob).can.write({'NEW_KEY':'test'}).path('classMentors/eventQuestions/'+bobEventId+"/questions");
-      // expect(auth.bob).can.read.path('classMentors/eventQuestions/'+bobEventId+"/questions");
+    it('should allow the event participants to read eventQuestions', function() {
+      expect(auth.bob).can.read.path(questionPath);
     });
 
-    it('should not allow non-event owner to read and write to eventQuestions', function() {
-      //expect(auth.bob).cannot.write({'test':'test'}).path('classMentors/eventQuestions/'+eventId);
-      //expect(auth.bob).cannot.read.path('classMentors/eventQuestions/'+eventId);
+    it('should disallow other user to read eventQuestions', function() {
+      expect(auth.emma).cannot.read.path(questionPath);
     });
+
+    // it('should allow the event owner to read questionOwners', function() {
+
+    //   // TODO: Find a matching rule and unit test.
+    //   // expect(auth.bob).can.read.path('classMentors/eventQuestions/'+bobEventId+'/questionOwners');
+    // });
+
+    // it('should allow the event owner to read and write to eventQuestions', function() {
+
+    //   // expect(auth.bob).can.write({'NEW_KEY':'test'}).path('classMentors/eventQuestions/'+bobEventId+"/questions");
+    //   // expect(auth.bob).can.read.path('classMentors/eventQuestions/'+bobEventId+"/questions");
+    // });
+
+    // it('should not allow non-event owner to read and write to eventQuestions', function() {
+
+    //   // expect(auth.bob).cannot.write({'test':'test'}).path('classMentors/eventQuestions/'+eventId);
+    //   // expect(auth.bob).cannot.read.path('classMentors/eventQuestions/'+eventId);
+    // });
 
     // Event participants should be able to read all questions for their event.
     // Non-event participants should not be able to read questions for an event.
