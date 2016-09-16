8f8a4b4affa2b9c56481499ac17a4e42f2636af5
Damien Lebrun <dinoboff@hotmail.com>
2016-09-16 17:51:40 +0100

    Set eventQuestions questions validation rules

--- database/test/eventQuestions.specs.js
+++ database/test/eventQuestions.specs.js
@@ -79,29 +79,36 @@ describe('eventQuestions', function() {
       addQuestion('emma-question-1', auth.emma, shouldFail);
     });
 
-    // it('should allow the event owner to read questionOwners', function() {
+    it('should disallow to add question on behalf of some other user', function() {
+      const path = `${questionsPath}/bob-question-2`;
+      const question = makeQuestion(auth.bob);
 
-    //   // TODO: Find a matching rule and unit test.
-    //   // expect(auth.bob).can.read.path('classMentors/eventQuestions/'+bobEventId+'/questionOwners');
-    // });
+      expect(auth.alice).cannot.write(question).to.path(path);
+    });
 
-    // it('should allow the event owner to read and write to eventQuestions', function() {
+    it('should disallow to add question without a title', function() {
+      const path = `${questionsPath}/alice-question-2`;
+      const question = makeQuestion(auth.alice);
 
-    //   // expect(auth.bob).can.write({'NEW_KEY':'test'}).path('classMentors/eventQuestions/'+bobEventId+"/questions");
-    //   // expect(auth.bob).can.read.path('classMentors/eventQuestions/'+bobEventId+"/questions");
-    // });
+      question.title = undefined;
+      expect(auth.alice).cannot.write(question).to.path(path);
 
-    // it('should not allow non-event owner to read and write to eventQuestions', function() {
+      question.title = '';
+      expect(auth.alice).cannot.write(question).to.path(path);
+    });
 
-    //   // expect(auth.bob).cannot.write({'test':'test'}).path('classMentors/eventQuestions/'+eventId);
-    //   // expect(auth.bob).cannot.read.path('classMentors/eventQuestions/'+eventId);
-    // });
+    it('should disallow to add question without a body', function() {
+      const path = `${questionsPath}/alice-question-2`;
+      const question = makeQuestion(auth.alice);
 
-    // Event participants should be able to read all questions for their event.
-    // Non-event participants should not be able to read questions for an event.
+      question.body = undefined;
+      expect(auth.alice).cannot.write(question).to.path(path);
+
+      question.body = '';
+      expect(auth.alice).cannot.write(question).to.path(path);
+    });
+
     // quesionOwners node should only be readable by the event owner.
-    // Event participants should only be able add/edit questions using their publicID.
-    // Event participants should only be able to add/edit answers with their publicID.
     // Event participants can comment on questions and answers
     // Event participants can upVote questions and answers
     // Question owners can select a winning answer.
