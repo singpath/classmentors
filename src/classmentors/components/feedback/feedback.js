130fd95335e58c211ecef4235e1fcfdb6f629167
shiquan-lim <shiquan.lim@gmail.com>
2016-10-10 11:05:40 +0800

    Enabled navigation to feedback partial

---
+++ src/classmentors/components/feedback/feedback.js
@@ -0,0 +1,69 @@
+/**
+ * Created by limshiquan on 10/10/16.
+ */
+/**
+ * classmentors/components/ace/ace.js - Define the ace component.
+ */
+import template from './feedback.html!text';
+
+/**
+ * Update navBar with a title and no action.
+ * @param {spfNavBarService} spfNavBarService
+ */
+function feedbackController(spfNavBarService) {
+    spfNavBarService.update('Feedback');
+}
+
+feedbackController.$inject = ['spfNavBarService'];
+
+/**
+ * ace component.
+ *
+ * @type {Object}
+ */
+export const component = {
+    template,
+    bindings: {
+
+        // binds $ctrl.stats to the value of the stats attribute.
+        stats: '<'
+    },
+    controller: feedbackController
+};
+
+// export const ACE_STATS_URL = 'https://dl.dropboxusercontent.com/u/4972572/ace_of_coders_2016_stats.json';
+
+/**
+ * Route resolver helper.
+ *
+ * This is not a service; this is not generating a singleton. If used in a route
+ * configuration "resolve" map, the function will be run on each resolution of
+ * that route.
+ *
+ * @param  {Object}  $http       $http service.
+ * @param  {string}  aceStatsUrl URL to fetch stats from
+ * @return {Promise}
+ */
+export function getStats($http, aceStatsUrl) {
+    return $http.get(aceStatsUrl).then(
+        response => response.data
+    );
+}
+getStats.$inject = ['$http', 'aceStatsUrl'];
+
+/**
+ * Config route for ace of coders views.
+ *
+ * @param  {object} $routeProvider ngRoute $route service provider.
+ * @param  {object} routes         classmentors route map.
+ */
+export function configRoute($routeProvider, routes) {
+    $routeProvider
+        .when(routes.feedback, {
+            template: template,
+            resolve: {stats: getStats}
+        })
+        .otherwise(routes.home);
+}
+
+configRoute.$inject = ['$routeProvider', 'routes'];
