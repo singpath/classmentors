0000669c7029cbf4965e7fe3c54612c36a9d47c8
shiquan-lim <shiquan.lim@gmail.com>
2016-10-10 15:34:27 +0800

    Completed feedback functionality

--- src/classmentors/components/feedback/feedback.js
+++ src/classmentors/components/feedback/feedback.js
@@ -1,55 +1,68 @@
 /**
  * Created by limshiquan on 10/10/16.
  */
-/**
- * classmentors/components/ace/ace.js - Define the ace component.
- */
 import template from './feedback.html!text';
 
 /**
  * Update navBar with a title and no action.
  * @param {spfNavBarService} spfNavBarService
  */
-function feedbackController(spfNavBarService) {
+function feedbackController(initialData, spfNavBarService, urlFor, firebaseApp, spfAlert, $firebaseObject) {
+    var self = this;
+    var db = firebaseApp.database();
+
+    this.currentUser = initialData.currentUser;
+    this.profile = initialData.profile;
+
+    this.feedbackType = null;
+    this.feedback = {};
+    this.anonymity = false;
+    this.allFeedback = {};
+
     spfNavBarService.update('Feedback');
+    console.log(initialData);
+
+    this.toggleFeedbackType = function (feedbackType) {
+        self.feedbackType = feedbackType;
+        self.feedback = {};
+        if(self.feedbackType=='view' && self.currentUser.$id=='HTIc4MVi5CWC4I9rsbM6x0AarN52') {
+            var path = db.ref('classMentors/userFeedback');
+            var fb = $firebaseObject(path);
+            fb.$loaded().then(function () {
+                self.allFeedback = fb;
+            });
+        }
+    };
+
+    this.saveFeedback = function () {
+        self.feedback.type = self.feedbackType;
+
+        if(!self.anonymity) {
+            self.feedback.loggedBy = {};
+            self.feedback.loggedBy.displayName = self.profile.user.displayName;
+            self.feedback.loggedBy.id = self.profile.$id;
+        } else {
+            self.feedback.loggedBy = "anonymous";
+        }
+
+        var ref = db.ref('classMentors/userFeedback');
+        ref.push(self.feedback).then(function () {
+                self.feedbackType = null;
+                self.feedback = {};
+        }).then(
+            spfAlert.success("Feedback successfully registered. Thank you!")
+        ).catch(function (error) {
+            spfAlert.error("error")
+        });
+    };
 }
 
-feedbackController.$inject = ['spfNavBarService'];
+feedbackController.$inject = ['initialData', 'spfNavBarService', 'urlFor', 'firebaseApp', 'spfAlert', '$firebaseObject'];
 
-/**
- * ace component.
- *
- * @type {Object}
- */
 export const component = {
     template,
-    bindings: {
-
-        // binds $ctrl.stats to the value of the stats attribute.
-        stats: '<'
-    },
     controller: feedbackController
 };
-
-// export const ACE_STATS_URL = 'https://dl.dropboxusercontent.com/u/4972572/ace_of_coders_2016_stats.json';
-
-/**
- * Route resolver helper.
- *
- * This is not a service; this is not generating a singleton. If used in a route
- * configuration "resolve" map, the function will be run on each resolution of
- * that route.
- *
- * @param  {Object}  $http       $http service.
- * @param  {string}  aceStatsUrl URL to fetch stats from
- * @return {Promise}
- */
-export function getStats($http, aceStatsUrl) {
-    return $http.get(aceStatsUrl).then(
-        response => response.data
-    );
-}
-getStats.$inject = ['$http', 'aceStatsUrl'];
 
 /**
  * Config route for ace of coders views.
@@ -61,9 +74,26 @@ export function configRoute($routeProvider, routes) {
     $routeProvider
         .when(routes.feedback, {
             template: template,
-            resolve: {stats: getStats}
+            controller: feedbackController,
+            controllerAs: 'ctrl',
+            resolve: {initialData: clmFeedbackResolver}
         })
         .otherwise(routes.home);
 }
 
 configRoute.$inject = ['$routeProvider', 'routes'];
+
+function clmFeedbackResolver($q, spfAuth, spfAuthData, clmDataStore) {
+    return $q.all({
+        featuredCohorts: clmDataStore.cohorts.listFeaturedCohorts(),
+        auth: spfAuth.$loaded(),
+        currentUser: spfAuthData.user().catch(function(error) {
+            return error;
+        }),
+        profile: clmDataStore.currentUserProfile(),
+        createdCohorts: clmDataStore.cohorts.listCreatedCohorts(),
+        joinedEvents: clmDataStore.events.listJoinedEvents(),
+        allCohorts: clmDataStore.cohorts.listAllCohorts()
+    });
+}
+clmFeedbackResolver.$inject = ['$q', 'spfAuth', 'spfAuthData', 'clmDataStore'];
