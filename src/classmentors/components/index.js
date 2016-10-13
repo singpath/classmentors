cfedb35cf5134c93e0cd309808e01cc1c8501937
shiquan-lim <shiquan.lim@gmail.com>
2016-10-13 21:48:46 +0800

    Enabled navigation to question queue partial

--- src/classmentors/components/index.js
+++ src/classmentors/components/index.js
@@ -5,6 +5,7 @@ import * as cohorts from 'classmentors/components/cohorts/cohorts.js';
 import * as events from 'classmentors/components/events/events.js';
 import * as profiles from 'classmentors/components/profiles/profiles.js';
 import * as feedback from 'classmentors/components/feedback/feedback.js';
+import * as questionQueue from 'classmentors/components/question-queue/question-queue.js';
 import admin from 'classmentors/components/admin/admin.js';
 import serviceCard from 'classmentors/components/service-card/service-card.js';
 
@@ -17,7 +18,8 @@ const components = {
   events,
   profiles,
   serviceCard,
-  feedback
+  feedback,
+  questionQueue
 };
 
 export default components;
