85736ab308f74341159645bcc0169363990b02c3
shiquan-lim <shiquan.lim@gmail.com>
2016-10-14 23:43:09 +0800

    Enabled navigation to event-specific queues

--- src/classmentors/components/events/events.js
+++ src/classmentors/components/events/events.js
@@ -1499,19 +1499,7 @@ function EditEventTaskCtrl(initialData, spfAlert, urlFor, spfNavBarService, clmD
         this.taskType = 'survey';
     }
 
-    this.toggle = function (item, list) {
-        var idx = list.indexOf(item);
-        if (idx > -1) {
-            list.splice(idx, 1);
-        }
-        else {
-            list.push(item);
-        }
-    };
 
-    this.exists = function (item, list) {
-        return list.indexOf(item) > -1;
-    };
     // else if (this.task.profileEdit) {
     //     return 'Save';
     // }
--- src/classmentors/components/question-queue/question-queue.js
+++ src/classmentors/components/question-queue/question-queue.js
@@ -2,6 +2,7 @@
  * Created by limshiquan on 13/10/16.
  */
 import qqHome from './question-queue.html!text';
+import eventQ from './event-queue.html!text';
 
 /**
  * Update navBar with a title and no action.
@@ -21,21 +22,61 @@ function qqController(initialData, spfNavBarService, urlFor, firebaseApp, spfAle
     var self = this;
     var db = firebaseApp.database();
 
-    this.currentUser = initialData.currentuser;
+    this.currentUser = initialData.currentUser;
     this.profile = initialData.profile;
     this.createdEvents = initialData.createdEvents;
     this.joinedEvents = initialData.joinedEvents;
 
-    spfNavBarService.update('Question Queues');
+    spfNavBarService.update('Question Queues', getOptions());
+
+    function getOptions() {
+        var options = [];
+
+        // options.push({
+        //     title: 'Add New Challenge',
+        //     url: `#${urlFor('events')}`,
+        //     icon: 'add-circle-outline'
+        // });
+
+        return options;
+    }
 
 }
 
 qqController.$inject = ['initialData', 'spfNavBarService', 'urlFor', 'firebaseApp', 'spfAlert', '$firebaseObject'];
 
-export const component = {
-    qqHome,
-    controller: qqController
-};
+function eventQController(initialData, spfNavBarService, urlFor, firebaseApp, spfAlert, $firebaseObject) {
+    var self = this;
+    var db = firebaseApp.database();
+
+    this.currentUser = initialData.currentUser;
+    this.profile = initialData.profile;
+    this.createdEvents = initialData.createdEvents;
+    this.joinedEvents = initialData.joinedEvents;
+
+    spfNavBarService.update('Question Queues', getOptions());
+
+    function getOptions() {
+        var options = [];
+
+        // options.push({
+        //     title: 'Add New Challenge',
+        //     url: `#${urlFor('events')}`,
+        //     icon: 'add-circle-outline'
+        // });
+
+        return options;
+    }
+
+}
+
+eventQController.$inject = ['initialData', 'spfNavBarService', 'urlFor', 'firebaseApp', 'spfAlert', '$firebaseObject'];
+
+// export const component = {
+//     qqHome,
+//     // controller: qqController,
+//     eventQ
+// };
 
 /**
  * Config route for ace of coders views.
@@ -50,6 +91,12 @@ export function configRoute($routeProvider, routes) {
             controller: qqController,
             controllerAs: 'ctrl',
             resolve: {initialData: clmQQResolver}
+        })
+        .when(routes.eventQueue, {
+            template: eventQ,
+            controller: eventQController,
+            controllerAs: 'ctrl',
+            resolve: {initialData: eventQResolver}
         })
         .otherwise(routes.home);
 }
@@ -68,3 +115,17 @@ function clmQQResolver($q, spfAuth, spfAuthData, clmDataStore) {
     });
 }
 clmQQResolver.$inject = ['$q', 'spfAuth', 'spfAuthData', 'clmDataStore'];
+
+function eventQResolver($q, spfAuth, spfAuthData, clmDataStore, $route) {
+    var eventId = $route.current.params.eventId;
+    return $q.all({
+        auth: spfAuth.$loaded(),
+        currentUser: spfAuthData.user().catch(function(error) {
+            return error;
+        }),
+        profile: clmDataStore.currentUserProfile(),
+        joinedEvents: clmDataStore.events.listJoinedEvents(),
+        createdEvents: clmDataStore.events.listCreatedEvents()
+    });
+}
+eventQResolver.$inject = ['$q', 'spfAuth', 'spfAuthData', 'clmDataStore', '$route'];
