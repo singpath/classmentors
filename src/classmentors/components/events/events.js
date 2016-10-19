03a21b4b18706f7e4ca126220e366d3f929b3bb8
Chris Boesch <cboesch@smu.edu.sg>
2016-10-19 12:09:57 +0800

    Extending ranking table participants query to handle large events.

--- src/classmentors/components/events/events.js
+++ src/classmentors/components/events/events.js
@@ -3242,7 +3242,7 @@ function ClmEventRankTableCtrl($log, $q, $filter, firebaseApp, $firebaseObject, 
 
     function setParticipants(ctrl) {
         var ref = db.ref(`classMentors/eventParticipants/${ctrl.event.$id}`);
-        var query = ref.limitToLast(100);
+        var query = ref.limitToLast(1000);
         var data = $firebaseArray(query);
 
         data.$loaded().then(function() {
