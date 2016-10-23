96ef60ba9307c221a681da8bb23ddd20c588f7cb
TAN WEI JIE, AMOS <twjAmos@gmail.com>
2016-10-23 18:29:14 +0800

    Fixed 'your team leader is' message

--- src/classmentors/components/challenges/teamActivity/teamactivity.js
+++ src/classmentors/components/challenges/teamActivity/teamactivity.js
@@ -150,7 +150,7 @@ createTeamActivityController.$inject = [
 ];
 
 function startTRATInitialData($q, spfAuthData, eventService, clmDataStore, firebaseApp, $firebaseObject, $firebaseArray, $route) {
-    
+
     var data = eventService.get();
     var db = firebaseApp.database();
 
@@ -321,7 +321,7 @@ function startTRATController($q, initialData, clmDataStore, $location, urlFor,
             timestamp: TIMESTAMP
         };
     }
-
+    
     var attempts = [];
     self.nextQuestion = function(){
         // For Single answer MCQ
