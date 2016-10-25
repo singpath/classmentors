0b7876b9dbafe24e7ddac77bb115bda0e14e78be
shiquan-lim <shiquan.lim@gmail.com>
2016-10-25 21:46:30 +0800

    Workign on fixing TRAT ranking order

--- src/classmentors/components/events/events.js
+++ src/classmentors/components/events/events.js
@@ -710,7 +710,6 @@ function ViewEventCtrl($scope, initialData, $document, $mdDialog, $route,
     this.loadSolutions = function () {
         self.loadingSolutions = true;
         console.log("loading solutions....");
-        console.log(self.selected);
         // self.solutions = clmDataStore.events.getSolutions(self.event.$id).then(function (solutions) {
         //     console.log(solutions);
         //     return solutions;
